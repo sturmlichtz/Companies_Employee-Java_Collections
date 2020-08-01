33951f0ef65f52e382fe5b13ae4e8a38f3f06970
sturmlichtz <roshanrbhoir@gmail.com>
2020-08-01 15:16:43 +0530

    add feature to display part time wage

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,14 +1,21 @@
 public class Employee_Wage_Computation {
 	public static void main(String[] args) {
-		final int wagePerHour=20;
-		final int fulldayHour=8;
-		int dailyWage=0;
 		System.out.println("Welcome to Employee Wage Computation");
+		final int wagePerHour=20;
+		final int fullDayHour=8;
+		final int partTimeHour=4;
+		int dailyWage=0;
 		int isFullTime=1;
-		int empCheck = (int)Math.floor(Math.random()*10)%2;
+		int isPartTime=2;
+		int empCheck = (int)Math.floor(Math.random()*10)%3;
 		if (empCheck == isFullTime){
-			System.out.println("Employee is Present");
-			dailyWage=(wagePerHour*fulldayHour);
+			System.out.println("Employee is Present for full time");
+			dailyWage=(wagePerHour*fullDayHour);
+			System.out.println("daily wage is "+dailyWage);
+		}
+		else if (empCheck == isPartTime) {
+			System.out.println("Employye is present for part time");
+			dailyWage=(wagePerHour*partTimeHour);
 			System.out.println("daily wage is "+dailyWage);
 		}
 		else{
