b9ba91554a008ba8487ca7a0e945aa04cce6bb2c
sturmlichtz <roshanrbhoir@gmail.com>
2020-08-01 13:12:54 +0530

    add check attendance

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,5 +1,13 @@
 public class Employee_Wage_Computation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int isFullTime=1;
+		int empCheck = (int)Math.floor(Math.random()*10)%2;
+		if (empCheck == isFullTime){
+			System.out.println("Employee is Present");
+		}
+		else{
+			System.out.println("Employee is absent");
+		}
 	}
 }
