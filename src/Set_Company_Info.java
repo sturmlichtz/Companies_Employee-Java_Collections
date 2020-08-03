2f624c77df8f6312c69a0bd703793b44b138e6bd
sturmlichtz <roshanrbhoir@gmail.com>
2020-08-03 18:01:14 +0530

    add setter method for employee wage

--- src/Set_Company_Info.java
+++ src/Set_Company_Info.java
@@ -3,10 +3,22 @@ public class Set_Company_Info {
 	int maxMonthDays;
 	int maxMonthHours;
 	int empRatePerHour;
+	int empMonthlyWage;
 	Set_Company_Info(String companyName, int empRatePerHour,int maxMonthDays,int maxMonthHours) {
 		this.companyName=companyName;
 		this.empRatePerHour=empRatePerHour;
 		this.maxMonthDays=maxMonthDays;
 		this.maxMonthHours=maxMonthHours;
+		empMonthlyWage=0;
 	}
+	void Set_Emp_Wage(int empMonthlyWage) {
+		this.empMonthlyWage=empMonthlyWage;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "total wage for comapany " +companyName+" is: "+empMonthlyWage ;
+	}
+	
 }
