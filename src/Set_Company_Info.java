774add827142e1e4e3469f60b68f343c4d7bff3f
sturmlichtz <roshanrbhoir@gmail.com>
2020-08-03 12:10:13 +0530

    add setter method for information of multiple companies

---
+++ src/Set_Company_Info.java
@@ -0,0 +1,12 @@
+public class Set_Company_Info {
+	String companyName;
+	int maxMonthDays;
+	int maxMonthHours;
+	int empRatePerHour;
+	Set_Company_Info(String companyName, int empRatePerHour,int maxMonthDays,int maxMonthHours) {
+		this.companyName=companyName;
+		this.empRatePerHour=empRatePerHour;
+		this.maxMonthDays=maxMonthDays;
+		this.maxMonthHours=maxMonthHours;
+	}
+}
