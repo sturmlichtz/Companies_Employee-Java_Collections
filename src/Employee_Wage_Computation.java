378219c09044d8222df96d7afdffb62f7586035e
sturmlichtz <roshanrbhoir@gmail.com>
2020-08-03 12:25:03 +0530

    refactor code to add comanies information into array

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 public class Employee_Wage_Computation {
+	Scanner sc = new Scanner(System.in);
 	final int isFullTime=1;
 	final int isPartTime=2;
 	final int partTimeHours=4;
 	final int fullTimeHours=8;
+	int noOfCompnay=0;
+	Set_Company_Info[] companyArray = new Set_Company_Info[2];	
 	private int emp_Random_Check() {
 		int empCheck=(int) Math.floor(Math.random()*10)%3;
 		return empCheck;
 	}
+	
 	private void welcome_message() {
 		System.out.println("Welcome to Employee Wage Computation Program");
 	}
+	
 	private void checkAttendence(){	
 		switch (emp_Random_Check()) {
 		case isFullTime:
@@ -23,47 +30,60 @@ public class Employee_Wage_Computation {
 			break;
 		}
 	}
-	private int daliy_Wage() {
-		final int empRatePerHour=20;
+	private int daliy_Wage(Set_Company_Info comapnayInfoRef) {	
 		int empWage=0;
 		switch (emp_Random_Check()) {
 		case isFullTime:
-			empWage = (empRatePerHour*fullTimeHours);	
+			empWage = (comapnayInfoRef.empRatePerHour*fullTimeHours);	
 			break;
 		case isPartTime:
-			empWage= (empRatePerHour*partTimeHours);
+			empWage= (comapnayInfoRef.empRatePerHour*partTimeHours);
 			break;
 		default:
 			break;
 		}
 		return empWage;
 	}
-	int monthly_Wage() {
-		final int maxHours=100;
-		final int maxDays=20;
+	int monthly_Wage(Set_Company_Info comapnayInfoRef) {
 		int currentDay = 0;
 		int currentHour=0;
 		int monthlyWage = 0;
-		while (currentHour<=maxHours || currentDay<=maxDays) {
+		while (currentHour<=comapnayInfoRef.maxMonthHours && currentDay<=comapnayInfoRef.maxMonthDays) {
+			currentDay++;
 			switch (emp_Random_Check()) {
 			case isFullTime:
-				monthlyWage+=daliy_Wage();
+				monthlyWage+=daliy_Wage(comapnayInfoRef);
 				currentHour+=fullTimeHours;
 				break;
 			case isPartTime:
-				monthlyWage+=daliy_Wage();
+				monthlyWage+=daliy_Wage(comapnayInfoRef);
 				currentHour+=partTimeHours;
 				break;
 			default:
 				break;
 			}
-			currentDay++;
 		}
 		return monthlyWage;
 	}
+	
+	public void add_Company_Info(String companyName, int empRatePerHour,int maxDays,int maxHours) {	
+		companyArray[noOfCompnay]= new Set_Company_Info(companyName,empRatePerHour,maxDays,maxHours);
+		noOfCompnay++;
+	}
+	
+	private void display() {
+		for (int i = 0; i < noOfCompnay; i++) {
+			int totawage = this.monthly_Wage(companyArray[i]);
+			System.out.println("Total Monthly Salary for "+companyArray[i].companyName+" is "+totawage);
+	
+		}
+	}
+
 	public static void main(String[] args) {
 		Employee_Wage_Computation employeeWageRef = new Employee_Wage_Computation();
 		employeeWageRef.welcome_message();
-		System.out.println("Monthly Wage is "+employeeWageRef.monthly_Wage()); 
+		employeeWageRef.add_Company_Info("D-Mart", 20, 20, 100);
+		employeeWageRef.add_Company_Info("Reliance", 20, 20, 100);
+		employeeWageRef.display();
 	}
 }
